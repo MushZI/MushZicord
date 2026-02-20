27e1c2fc48bb744178f75df4b22110b939d836ca
MushZI <88376339+MushZI@users.noreply.github.com>
2026-02-20 13:16:07 +0100

    test

--- browser/VencordNativeStub.ts
+++ browser/VencordNativeStub.ts
@@ -135,10 +135,4 @@ window.VencordNative = {
 
     pluginHelpers: {} as any,
     csp: {} as any,
-    tray: {
-        setUpdateState: NOOP,
-        onCheckUpdates: NOOP,
-        onRepair: NOOP,
-        onAbout: NOOP,
-    },
 };
